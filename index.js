3f297f015230739ba689dbfa26670113a9919b4a
Ahmed Taha <ataha7054@gmail.com>
2026-05-16 06:17:27 +0300

    fix cros 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 const PORT = 3000;
